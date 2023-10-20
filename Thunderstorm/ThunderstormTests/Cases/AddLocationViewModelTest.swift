d1637e84b39e59c368798720b32cdca1794cbd5d
Ygor Nascimento <ygor.nascimento@outlook.com>
2023-10-20 08:44:24 -0300

    37 - The Challenges of Asynchronous Unit Tests

--- Thunderstorm/ThunderstormTests/Cases/AddLocationViewModelTest.swift
+++ Thunderstorm/ThunderstormTests/Cases/AddLocationViewModelTest.swift
@@ -12,9 +12,14 @@ import Combine
 
 final class AddLocationViewModelTest: XCTestCase {
     
+    private enum AddLocationResult{
+        case success
+        case failure
+    }
+    
     private var subscriptions:  Set<AnyCancellable> = []
     
-    override class func tearDown() throws {
+    override func tearDownWithError() throws {
         try super.tearDownWithError()
         
         subscriptions.removeAll()
@@ -30,12 +35,20 @@ final class AddLocationViewModelTest: XCTestCase {
         XCTAssertEqual(textFieldPlaceholder, "Enter the name of a city...")
     }
     
+    func testAddLocationSuccess() async throws {
+        try await runAddLocationTest(result: .success)
+    }
+    
+    func testAddLocationFailure() async throws {
+        try await runAddLocationTest(result: .failure)
+    }
+    
     @MainActor
-    func testAddLocation() async throws {
-        let locationToAdd = Location.mock
+    private func runAddLocationTest(result: AddLocationResult) async throws {
+        let locationToAddID = result == .success ? Location.mock.id : "abc"
         
         let store = MockStore()
-        let geocodingService = MockGeocodingClient(result: .success([locationToAdd]))
+        let geocodingService = MockGeocodingClient(result: .success([Location.mock]))
         
         let viewModel = AddLocationViewModel(store: store,
                                              geocodingService: geocodingService)
@@ -43,14 +56,24 @@ final class AddLocationViewModelTest: XCTestCase {
         XCTAssertTrue(store.locations.isEmpty)
         
         let expectation = expectation(description: "Validade Locations")
+        expectation.isInverted = result == .failure
         
         store.locationsPublisher
             .filter { locations in
-                locations.contains { $0.id == locationToAdd.id }
+                locations.contains { $0.id == locationToAddID }
             }
             .sink { _ in
                 expectation.fulfill()
             }.store(in: &subscriptions)
+        
+        viewModel.$locations
+            .sink { _ in
+                DispatchQueue.main.async {
+                    viewModel.addLocation(with: locationToAddID)
+                }
+            }.store(in: &subscriptions)
+        
+        viewModel.query = "Brussels"
         
         await fulfillment(of: [expectation], timeout: 10.0)
     }
